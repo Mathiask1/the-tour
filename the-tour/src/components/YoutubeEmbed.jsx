d2fe71a597d3590765bb7d467765c709c8d2d78d
Mathiask1 <birkebjerg99@live.dk>
2024-05-04 18:47:20 +0200

    added id and fix border

--- the-tour/src/components/YoutubeEmbed.jsx
+++ the-tour/src/components/YoutubeEmbed.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../styles/Frontpage.css";
 
 const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
+  <div id="youtube-vid-div" >
     <iframe
+      id="youtube-vid-iframe"
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
