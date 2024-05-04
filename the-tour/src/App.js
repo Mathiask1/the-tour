00d55d2db2573bc3e4e49aab0b351ab64a9cc5af
Mathiask1 <birkebjerg99@live.dk>
2024-05-04 18:30:57 +0200

    Update to avoid conflict with cfgorbat maker

--- the-tour/src/App.js
+++ the-tour/src/App.js
@@ -8,10 +8,10 @@ function App() {
     <div className='app'>
       <Router>
         <Routes>
-        <Route path="/the-tour" element={<Home />} />
-        <Route path="/" element={<Home />} />
-          <Route path="/404" element={<NotFoundPage />} />
-          <Route path="*" element={<Navigate to="/404" />} />
+          <Route path="/the-tour" element={<Home />} />
+          <Route path="/the-tour/" element={<Home />} />
+          <Route path="/the-tour/404" element={<NotFoundPage />} />
+          <Route path="/the-tour/*" element={<Navigate to="/the-tour/404" />} />
         </Routes>
       </Router>
     </div>
